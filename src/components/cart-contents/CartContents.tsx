eba96faa883bfadb276c575211adfe71aa44f334
zeusz3 <zeus3droid@gmail.com>
2026-05-03 13:38:55 +0200

    Renamed /products to /product
    Path corrected to match QR codes in circulation

--- src/components/cart-contents/CartContents.tsx
+++ src/components/cart-contents/CartContents.tsx
@@ -58,7 +58,7 @@ export default component$<{
 								<div>
 									<div class="flex justify-between text-base font-medium text-gray-900">
 										<h3>
-											<a href={`/products/${line.productVariant.product.slug}/`}>
+											<a href={`/product/${line.productVariant.product.slug}/`}>
 												{line.productVariant.name}
 											</a>
 										</h3>
